8a21b03ebd2bc5d4141157af9054b7a7bb9d2655
noahhkim <noahkim03@gmail.com>
2016-10-06 21:50:56 -0700

    Updates to Catalog Activity

--- app/src/main/java/com/example/android/deadstock/CatalogActivity.java
+++ app/src/main/java/com/example/android/deadstock/CatalogActivity.java
@@ -1,15 +1,25 @@
 package com.example.android.deadstock;
 
+import android.content.ContentUris;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.example.android.deadstock.data.ShoeContract.ShoeEntry;
 
 /** Displays list of shoes that were entered and stored in the app */
 public class CatalogActivity extends AppCompatActivity implements
@@ -42,20 +52,116 @@ public class CatalogActivity extends AppCompatActivity implements
         // Find and set empty iew on the ListView, so that it only shows when the list has 0 items
         View emptyView = findViewById(R.id.empty_view);
         shoeListView.setEmptyView(emptyView);
+
+        // Setup on Adapter to create a list item for each row of shoe data in the Cursor
+        // There is no shoe data yet (until the loader finishes) so pass in null for the Cursor
+        mCursorAdapter = new ShoeCursorAdapter(this, null);
+        shoeListView.setAdapter(mCursorAdapter);
+
+        // Setup item click listener
+        shoeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+
+                // Form the content URI that represents the specific shoe that was clicked on
+                Uri currentShoeUri = ContentUris.withAppendedId(ShoeEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentShoeUri);
+
+                // Launch the EditorActivity to display data for current shoe
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return null;
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Adds menu items to app bar
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Delete All Shoes" menu option
+            case R.id.action_delete_all_shoes:
+                // Delete all pets from the database
+                showDeleteConfirmationDialog();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deleteAllShoes() {
+        getContentResolver().delete(ShoeEntry.CONTENT_URI, null, null);
+    }
+
+    private void showDeleteConfirmationDialog() {
+        // Create an AlertDialog.Builder and set the message and click listeners
+        // for hte positive and negative buttons on the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_all_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // User clicked the "Delete" button, so delete all shoes
+                deleteAllShoes();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked the "Cancel" button, so dismiss the dialog
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int loaderID, Bundle bundle) {
+
+        // Define a projection that specifies which columns from the database
+        // we will actually use after this query
+        String[] projection = {
+                ShoeEntry._ID,
+                ShoeEntry.COLUMN_SHOE_NAME,
+                ShoeEntry.COLUMN_SHOE_QUANTITY,
+                ShoeEntry.COLUMN_SHOE_PRICE};
+
+        return new CursorLoader(
+                this,
+                ShoeEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
+        // Update {@Link ShoeCursorAdapter} with this new cursor containing updated shoe data
+        mCursorAdapter.swapCursor(data);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
+        // Callback called when the data needs to be deleted
+        mCursorAdapter.swapCursor(null);
 
     }
 }
--- app/src/main/java/com/example/android/deadstock/ShoeCursorAdapter.java
+++ app/src/main/java/com/example/android/deadstock/ShoeCursorAdapter.java
@@ -2,9 +2,14 @@ package com.example.android.deadstock;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.deadstock.data.ShoeContract.ShoeEntry;
 
 /**
  * Created by noahkim on 10/4/16.
@@ -18,11 +23,39 @@ public class ShoeCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views to modify in list item layout
+        TextView nameView = (TextView) view.findViewById(R.id.name);
+        TextView quantityView = (TextView) view.findViewById(R.id.quantity);
+        TextView priceView = (TextView) view.findViewById(R.id.price);
 
+        // Find columns of pet attributes
+        int nameColumnIndex = cursor.getColumnIndex(ShoeEntry.COLUMN_SHOE_NAME);
+        int quantityColumnIndex = cursor.getColumnIndex(ShoeEntry.COLUMN_SHOE_QUANTITY);
+        int priceColumnIndex = cursor.getColumnIndex(ShoeEntry.COLUMN_SHOE_PRICE);
+
+        // Read shoe attributes from Cursor for current shoe
+        String shoeName = cursor.getString(nameColumnIndex);
+        String shoeQuantity = cursor.getString(quantityColumnIndex);
+        String shoePrice = cursor.getString(priceColumnIndex);
+
+        // If quantity is an empty string or null, then default text says "N/A"
+        if (TextUtils.isEmpty(shoeQuantity)) {
+            shoeQuantity = "N/A";
+        }
+
+        // If price is an empty string or null, then default text says "N/A"
+        if (TextUtils.isEmpty(shoePrice)) {
+            shoePrice = "N/A";
+        }
+
+        // Update the TexViews with the attributes for the current shoe
+        nameView.setText(shoeName);
+        quantityView.setText(shoeQuantity);
+        priceView.setText(shoePrice);
     }
 }
